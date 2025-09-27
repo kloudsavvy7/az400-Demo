f5b8490664883d1c8f15922720d8641f624a51cd
KloudSavvy <120475908+kloudsavvy7@users.noreply.github.com>
2025-09-27 18:23:50 +0530

    Update EnableDiskOnline.ps1

--- EnableDiskOnline.ps1
+++ EnableDiskOnline.ps1
@@ -1,3 +1,3 @@
 diskpart /s "$env:ProgramData\Microsoft Azure\Scripts\Diskonline.txt" > "$env:ProgramData\Microsoft Azure\Scripts\EnableDisk.log"
 
-write-output "Ado demo "
+write-output "Ado demo"
